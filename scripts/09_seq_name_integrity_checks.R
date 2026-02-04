7377f406cdfdadcfe03933e09b4335f582b009ed
Chris Bird <cbird808@gmail.com>
2026-02-03 22:55:18 -0600

    TODO 09 FIX

--- scripts/09_seq_name_integrity_checks.R
+++ scripts/09_seq_name_integrity_checks.R
@@ -15,7 +15,10 @@ dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 yes_values <- c("yes", "y", "true", "1")
 
 #### LOAD INPUTS ####
-library_raw <- readxl::read_excel("data/Och_SSLibrariesforCapture_metadata.xlsx")
+library_raw <- readxl::read_excel(
+  "data/Och_SSLibrariesforCapture_metadata.xlsx",
+  na = c("", "NA", "N/A", "n/a")
+)
 
 #### BUILD BASE TABLE ####
 library_base <- 
@@ -38,38 +41,19 @@ library_base <-
   mutate(
     pool_round_lower = str_to_lower(as.character(pool_round)),
     in_test_lane_flag = str_to_lower(as.character(in_test_lane)) %in% yes_values,
-    in_full_lane_flag = str_to_lower(as.character(in_full_lane)) %in% yes_values,
-    run_type = case_when(
-      str_detect(pool_round_lower, "test lane 2") ~ "test_lane_2",
-      str_detect(pool_round_lower, "test lane") ~ "test_lane",
-      str_detect(pool_round_lower, "full") ~ "full_lane",
-      in_full_lane_flag ~ "full_lane",
-      in_test_lane_flag ~ "test_lane",
-      TRUE ~ NA_character_
-    ),
-    pool_id = case_when(
-      run_type %in% c("test_lane", "test_lane_2") ~ as.character(test_lane_pool),
-      run_type == "full_lane" ~ as.character(full_lane_pool),
-      TRUE ~ NA_character_
-    ),
-    run_key = case_when(
-      !is.na(pool_round) & as.character(pool_round) != "" ~ paste(run_type, pool_round, sep = "|"),
-      !is.na(run_type) ~ run_type,
-      TRUE ~ NA_character_
-    )
+    in_full_lane_flag = str_to_lower(as.character(in_full_lane)) %in% yes_values
   )
 
-#### DUPLICATES BY POOL ####
-pool_duplicates_long <- 
+#### BUILD LONG TABLE ####
+seq_long <- 
   library_base %>%
-  filter(!is.na(run_type), !is.na(pool_id)) %>%
   mutate(
     novogene_seq_id_all = novogene_seq_id,
     sequence_id_all = sequence_id,
     full_seq_name_all = full_seq_name,
     full_seq_decode_all = full_seq_decode,
-    sequence_id = na_if(sequence_id, ""),
     novogene_seq_id = na_if(novogene_seq_id, ""),
+    sequence_id = na_if(sequence_id, ""),
     full_seq_name = na_if(full_seq_name, ""),
     full_seq_decode = na_if(full_seq_decode, "")
   ) %>%
@@ -78,11 +62,28 @@ pool_duplicates_long <-
     names_to = "seq_field",
     values_to = "seq_value"
   ) %>%
-  filter(!is.na(seq_value))
+  mutate(
+    run_type = case_when(
+      seq_field %in% c("novogene_seq_id", "sequence_id") &
+        str_detect(pool_round_lower, "test lane 2") ~ "test_lane_2",
+      seq_field %in% c("novogene_seq_id", "sequence_id") &
+        str_detect(pool_round_lower, "test lane") ~ "test_lane",
+      seq_field %in% c("full_seq_name", "full_seq_decode") ~ "full_lane",
+      TRUE ~ NA_character_
+    ),
+    pool_id = case_when(
+      run_type %in% c("test_lane", "test_lane_2") ~ as.character(test_lane_pool),
+      run_type == "full_lane" ~ as.character(full_lane_pool),
+      TRUE ~ NA_character_
+    )
+  ) %>%
+  filter(!is.na(run_type))
 
+#### DUPLICATES BY POOL ####
 pool_duplicates <- 
-  pool_duplicates_long %>%
-  group_by(run_type, pool_round, pool_id, seq_field, seq_value) %>%
+  seq_long %>%
+  filter(!is.na(pool_id), !is.na(seq_value)) %>%
+  group_by(run_type, pool_id, seq_field, seq_value) %>%
   summarise(
     n_libraries = n(),
     library_ids = paste(unique(library_id), collapse = ";"),
@@ -97,29 +98,10 @@ pool_duplicates <-
   filter(n_libraries > 1)
 
 #### DUPLICATES BY RUN ####
-run_duplicates_long <- 
-  library_base %>%
-  filter(!is.na(run_key)) %>%
-  mutate(
-    novogene_seq_id_all = novogene_seq_id,
-    sequence_id_all = sequence_id,
-    full_seq_name_all = full_seq_name,
-    full_seq_decode_all = full_seq_decode,
-    sequence_id = na_if(sequence_id, ""),
-    novogene_seq_id = na_if(novogene_seq_id, ""),
-    full_seq_name = na_if(full_seq_name, ""),
-    full_seq_decode = na_if(full_seq_decode, "")
-  ) %>%
-  pivot_longer(
-    cols = c(novogene_seq_id, sequence_id, full_seq_name, full_seq_decode),
-    names_to = "seq_field",
-    values_to = "seq_value"
-  ) %>%
-  filter(!is.na(seq_value))
-
 run_duplicates <- 
-  run_duplicates_long %>%
-  group_by(run_type, pool_round, run_key, seq_field, seq_value) %>%
+  seq_long %>%
+  filter(!is.na(seq_value)) %>%
+  group_by(run_type, seq_field, seq_value) %>%
   summarise(
     n_libraries = n(),
     library_ids = paste(unique(library_id), collapse = ";"),
@@ -134,24 +116,10 @@ run_duplicates <-
   filter(n_libraries > 1)
 
 #### SUMMARY COUNTS ####
-pool_summary_long <- 
-  library_base %>%
-  filter(!is.na(run_type), !is.na(pool_id)) %>%
-  mutate(
-    novogene_seq_id = na_if(novogene_seq_id, ""),
-    sequence_id = na_if(sequence_id, ""),
-    full_seq_name = na_if(full_seq_name, ""),
-    full_seq_decode = na_if(full_seq_decode, "")
-  ) %>%
-  pivot_longer(
-    cols = c(novogene_seq_id, sequence_id, full_seq_name, full_seq_decode),
-    names_to = "seq_field",
-    values_to = "seq_value"
-  )
-
 pool_summary <- 
-  pool_summary_long %>%
-  group_by(run_type, pool_round, pool_id, seq_field) %>%
+  seq_long %>%
+  filter(!is.na(pool_id)) %>%
+  group_by(run_type, pool_id, seq_field) %>%
   summarise(
     n_libraries = n(),
     n_missing = sum(is.na(seq_value)),
@@ -160,7 +128,7 @@ pool_summary <-
 
 pool_duplicate_counts <- 
   pool_duplicates %>%
-  group_by(run_type, pool_round, pool_id, seq_field) %>%
+  group_by(run_type, pool_id, seq_field) %>%
   summarise(
     n_duplicate_values = n(),
     n_libraries_in_duplicates = sum(n_libraries),
@@ -169,31 +137,16 @@ pool_duplicate_counts <-
 
 pool_summary_final <- 
   pool_summary %>%
-  left_join(pool_duplicate_counts, by = c("run_type", "pool_round", "pool_id", "seq_field")) %>%
+  left_join(pool_duplicate_counts, by = c("run_type", "pool_id", "seq_field")) %>%
   mutate(
     level = "pool",
     n_duplicate_values = replace_na(n_duplicate_values, 0),
     n_libraries_in_duplicates = replace_na(n_libraries_in_duplicates, 0)
   )
 
-run_summary_long <- 
-  library_base %>%
-  filter(!is.na(run_key)) %>%
-  mutate(
-    novogene_seq_id = na_if(novogene_seq_id, ""),
-    sequence_id = na_if(sequence_id, ""),
-    full_seq_name = na_if(full_seq_name, ""),
-    full_seq_decode = na_if(full_seq_decode, "")
-  ) %>%
-  pivot_longer(
-    cols = c(novogene_seq_id, sequence_id, full_seq_name, full_seq_decode),
-    names_to = "seq_field",
-    values_to = "seq_value"
-  )
-
 run_summary <- 
-  run_summary_long %>%
-  group_by(run_type, pool_round, run_key, seq_field) %>%
+  seq_long %>%
+  group_by(run_type, seq_field) %>%
   summarise(
     n_libraries = n(),
     n_missing = sum(is.na(seq_value)),
@@ -202,7 +155,7 @@ run_summary <-
 
 run_duplicate_counts <- 
   run_duplicates %>%
-  group_by(run_type, pool_round, run_key, seq_field) %>%
+  group_by(run_type, seq_field) %>%
   summarise(
     n_duplicate_values = n(),
     n_libraries_in_duplicates = sum(n_libraries),
@@ -211,7 +164,7 @@ run_duplicate_counts <-
 
 run_summary_final <- 
   run_summary %>%
-  left_join(run_duplicate_counts, by = c("run_type", "pool_round", "run_key", "seq_field")) %>%
+  left_join(run_duplicate_counts, by = c("run_type", "seq_field")) %>%
   mutate(
     level = "run",
     pool_id = NA_character_,
@@ -221,8 +174,6 @@ run_summary_final <-
   select(
     level,
     run_type,
-    pool_round,
-    run_key,
     pool_id,
     seq_field,
     n_libraries,
@@ -234,12 +185,9 @@ run_summary_final <-
 summary_final <- 
   bind_rows(
     pool_summary_final %>%
-      mutate(run_key = NA_character_) %>%
       select(
         level,
         run_type,
-        pool_round,
-        run_key,
         pool_id,
         seq_field,
         n_libraries,
